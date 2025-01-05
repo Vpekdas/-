3d89e94b3872a16af6db70d2eb219a42e6c0a688
Vpekdas <volkan.pekdas@hotmail.com>
2025-01-05 16:51:33 +0100

    well we can now hover :)

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ require("dotenv").config();
 const email = process.env.email;
 const password = process.env.password;
 
-function delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+function randomDelay(min, max) {
+    return new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * (max - min + 1)) + min));
 }
 
 async function loginWithPassword(driver) {
+    await randomDelay(3000, 7000);
+
     await driver.findElement(By.name("email")).sendKeys(email);
     await driver.findElement(By.name("password")).sendKeys(password);
 
@@ -20,15 +22,15 @@ async function loginWithPassword(driver) {
         )
     );
 
-    await delay(5000);
-
     await loginBtn.click();
+
+    await randomDelay(3000, 7000);
 
     const authentif = prompt("Write 6 digit code:\n");
     await driver.findElement(By.className("inputDefault_f8bc55 input_f8bc55")).sendKeys(authentif);
 
     // ! The button is disabled until all 6 digits are entered, so a delay is added to ensure the button becomes enabled.
-    await delay(5000);
+    await randomDelay(3000, 7000);
 
     let confirmBtn = await driver.findElement(
         By.className("button_dd4f85 lookFilled_dd4f85 colorBrand_dd4f85 sizeMedium_dd4f85 grow_dd4f85")
@@ -39,21 +41,52 @@ async function loginWithPassword(driver) {
 
 async function main() {
     let driver = await new Builder().forBrowser(Browser.SAFARI).build();
-    driver.manage().window().setRect({ width: 1280, height: 720 });
+    driver.manage().window().setRect({ width: 1280, height: 720, x: 0, y: 0 });
 
     try {
         await driver.get("https://discord.com/login");
 
         const method = prompt("Do you want to login with QR code ?\n");
 
-        if (method === "NO") {
+        if (method === "no") {
             await loginWithPassword(driver);
         }
 
         await driver.wait(until.titleContains("Friends"));
-    } finally {
+
+        await randomDelay(3000, 7000);
+
+        const discordServer = await driver.findElement(By.className("blobContainer_a5ad63"));
+        await discordServer.click();
+
+        await randomDelay(3000, 7000);
+
+        const messageId = prompt("Please enter the id of the message.\n");
+
+        const messageToReact = await driver.findElement(By.id("message-content-" + messageId));
+
+        if (messageToReact) {
+            console.log("Message Found !");
+        }
+
+        await randomDelay(3000, 7000);
+
+        const actions = driver.actions({ async: true });
+        await actions.move({ origin: messageToReact }).perform();
+
+        await randomDelay(3000, 7000);
+
         prompt("Script ended...");
-        await driver.quit();
+    } catch (error) {
+        console.error("An error occurred:", error);
+    } finally {
+        if (driver) {
+            try {
+                await driver.quit();
+            } catch (quitError) {
+                console.error("Error quitting the driver:", quitError);
+            }
+        }
     }
 }
 
